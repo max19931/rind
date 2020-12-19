311a83a119cbef2739e7078305c1c378a3eca2c5
max19931 <53226461+max19931@users.noreply.github.com>
2020-12-19 10:36:41 +0100

    remove comments from sourcecode
    code is self explaining

--- dnsmessage.go
+++ dnsmessage.go
@@ -1,17 +1,9 @@
-// Copyright 2018 The Rind Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package rind
-
 import (
 	"fmt"
 	"strings"
-
 	"golang.org/x/net/dns/dnsmessage"
 )
-
-// question to string
 func qString(q dnsmessage.Question) string {
 	b := make([]byte, q.Name.Length+2)
 	for i := 0; i < int(q.Name.Length); i++ {
@@ -19,11 +11,8 @@ func qString(q dnsmessage.Question) string {
 	}
 	b[q.Name.Length] = uint8(q.Type >> 8)
 	b[q.Name.Length+1] = uint8(q.Type)
-
 	return string(b)
 }
-
-// resource name and type to string
 func ntString(rName dnsmessage.Name, rType dnsmessage.Type) string {
 	b := make([]byte, rName.Length+2)
 	for i := 0; i < int(rName.Length); i++ {
@@ -31,21 +20,15 @@ func ntString(rName dnsmessage.Name, rType dnsmessage.Type) string {
 	}
 	b[rName.Length] = uint8(rType >> 8)
 	b[rName.Length+1] = uint8(rType)
-
 	return string(b)
 }
-
-// resource to string
 func rString(r dnsmessage.Resource) string {
 	var sb strings.Builder
 	sb.Write(r.Header.Name.Data[:])
 	sb.WriteString(r.Header.Type.String())
 	sb.WriteString(r.Body.GoString())
-
 	return sb.String()
 }
-
-// packet to string
 func pString(p Packet) string {
 	return fmt.Sprint(p.message.ID)
 }
